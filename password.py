a08ce53db53807802ffd4611541e69569b59e7e7
Luke Griggs <lukegriggs@Lukes-MacBook-Air.local>
2023-10-07 15:23:20 -0500

    created functions that generate random letters, numbers, and symbols

--- password.py
+++ password.py
@@ -1,3 +1,24 @@
+import random
+
+def randnum():
+    min = 1
+    max = 10
+    randnum = random.randint(min, max)
+    return randnum
+
+def randUpperChar():
+    randchar = chr(random.randint(65, 90))
+    return randUpperChar
+
+def randLowerChar():
+    randLowerChar = chr(random.randint(97, 122))
+    return randLowerChar
+
+def randSymbol():
+    Symbols = "!@#$%?"
+    randSymbol = random.choice(Symbols)
+    return randSymbol
+
 
 
     
